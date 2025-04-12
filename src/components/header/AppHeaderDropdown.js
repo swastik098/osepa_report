374c0f7305e7de65a8f7a05c53a9135c7286160b
Debaswini Mishra <115451804+Debaswinimishra@users.noreply.github.com>
2025-04-12 15:50:44 +0530

    changes in logout function

--- src/components/header/AppHeaderDropdown.js
+++ src/components/header/AppHeaderDropdown.js
@@ -2,14 +2,33 @@ import React from 'react'
 import { CAvatar, CDropdown, CDropdownItem, CDropdownMenu, CDropdownToggle } from '@coreui/react'
 import CIcon from '@coreui/icons-react'
 import { cilLockLocked } from '@coreui/icons'
-
+import Swal from 'sweetalert2' // Import SweetAlert
 import avatar8 from './../../assets/images/user.jpg'
+import { useDispatch } from 'react-redux'
 
 const AppHeaderDropdown = () => {
+  const dispatch = useDispatch()
+  // const handleLogout = () => {
+  //   // Your logout logic here
+  //   console.log('User logged out')
+  //   // e.g., localStorage.clear(); navigate('/login');
+  // }
+
   const handleLogout = () => {
-    // Your logout logic here
-    console.log('User logged out')
-    // e.g., localStorage.clear(); navigate('/login');
+    Swal.fire({
+      title: 'Are you sure?',
+      text: 'Do you want to log out?',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3f51b5',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, log me out!',
+    }).then((result) => {
+      if (result.isConfirmed) {
+        navigate('/login')
+        localStorage.clear()
+      }
+    })
   }
 
   return (
